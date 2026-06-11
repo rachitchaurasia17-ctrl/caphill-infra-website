fde05765df0f2563876530d4a48d1f72ac846615
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:34:35 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for CAPHILL IN
 
 Firm facts:
 - Firm: CAPHILL INFRA
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Corporate Suite 102, Kishanpura-Sanauli Road, Zirakpur, Punjab 140603
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
